532217c9696f75f08593c3f4037d807240435c89
tvanrys <tvanrys@fanshaweonline.ca>
2018-06-25 11:53:32 -0400

    testing...

--- js/modules/main_VM.js
+++ js/modules/main_VM.js
@@ -10,6 +10,47 @@ const halvasVM = {
 
         methods : {
 
+            retrievePortfolioImages(queryType, cb) {
+                // set some variables and make this modular - if it's a lightbox call, then get one. if it's the initial load, get everything and populate the portfolio thumbs and add event handling
+                // if (queryType === 'getOne') {
+                //   let data = { $tableName : 'portfolio' , $image : }
+                // }
+            
+                // try a promise here after jquery ajax call is working
+                $.ajax({
+                  url  : 'admin/portfolioContent.php',
+                  type : 'GET',
+                  data : { $tableName : 'portfolio' }
+                })
+            
+                .done(function(data) {
+                  // need to validate the data here to make sure it's a vaild result?
+                  data = JSON.parse(data);
+            
+                  if (cb) { cb(data); }
+                })
+            
+                .fail(function(call, status, error) {
+                  // flash error messaging should go here
+                  console.log(error);
+                  console.dir(call);
+                });
+            },
+
+            renderPortfolioImages(data) {            
+                // push the ajax result into the portfolio VM data object
+                data.forEach(function(item, index) {
+                    halvasVM.vm.portfolioItems.push(item);
+                });
+            },
+            
+            init : function() {
+                // stub
+                console.log('welcome');
+            
+                this.retrievePortfolioImages('getAll', this.renderPortfolioImages);
+            }
+
         }
-    })
-}
+    }) // end vue VM
+};
